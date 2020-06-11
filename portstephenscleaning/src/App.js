6e3162c35b76095df7ef08b0345d7094788ce9a6
andressalh <andressalhenriques@hotmail.com>
2020-06-11 17:55:07 -0300

    section about, why us, testemunial

---
+++ portstephenscleaning/src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import Routes from './routes';
+
+
+function App() {
+  return (
+    <Fragment>
+      <Routes/>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ portstephenscleaning/src/pages/Home/index.js
@@ -0,0 +1,103 @@
+import React, { Fragment } from 'react';
+import {
+    Nav,
+    NavbarBrand,
+    NavItem,
+    NavLink,
+    Navbar,
+    CardImg,
+    Card,
+    CardBody,
+    CardText,
+    CardTitle,
+} from 'reactstrap';
+import logo from '../../assets/logo.png';
+import eco from '../../assets/eco.png';
+import me from '../../assets/me.jpg';
+import './styles.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
+
+export default function Home (){
+    return(
+        <Fragment>
+            <div className="header">
+                <Navbar  sticky="top"  expand="md">
+                    <Nav>
+                        <NavItem>
+                            <NavbarBrand href="/"><img className="logo" src={logo} alt="Logo" /></NavbarBrand>
+                        </NavItem>
+                    </Nav>
+                    <Nav className="links">
+                    <NavItem>
+                        <NavLink href="#about">About</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#services">Services</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#faq">FAQ'S</NavLink>
+                    </NavItem>  
+                    </Nav>
+                </Navbar >
+            </div>
+
+            <div id="about">
+            <h2>About Us</h2>
+            <p>We're a small family-owned and -operated business located in Nelson Bay-Corlette, and are proud to be relied upon by local and out-of-town property owners to provide detailed, committed and reliable home, AirBnB and commercial cleaning and laundry services in the Port Stephens area. We happily go the extra mile with any number of supplementary tasks that most don't even consider! If you need someone to take care of all the things you can't (or just don't have time to), call us today for a chat, and we can arrange to arrive at your location weekly, fortnightly or monthly.</p>
+            </div>
+
+            <div id="why-us">
+                <h2>Why us</h2>
+                <div className="row row-why-us">
+                    <Card className="card-why-us" >
+                        <CardImg variant="top" src={eco} />
+                        <CardBody>
+                            <CardTitle>Environmentally Friendly</CardTitle>
+                            <CardText>
+                                Have a chemical sensitivity or want to help the planet? We have environmentally friendly cleaning products and techniques and can adjust to meet your needs.
+                            </CardText>
+                        </CardBody>
+                    </Card>
+                    <Card className="card-why-us" >
+                        <CardImg variant="top" src={eco} />
+                        <CardBody>
+                            <CardTitle>Environmentally Friendly</CardTitle>
+                            <CardText>
+                                Have a chemical sensitivity or want to help the planet? We have environmentally friendly cleaning products and techniques and can adjust to meet your needs.
+                            </CardText>
+                        </CardBody>
+                    </Card>
+                    <Card className="card-why-us" >
+                        <CardImg variant="top" src={eco} />
+                        <CardBody>
+                            <CardTitle>Environmentally Friendly</CardTitle>
+                            <CardText>
+                                Have a chemical sensitivity or want to help the planet? We have environmentally friendly cleaning products and techniques and can adjust to meet your needs.
+                            </CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+            </div>
+       
+            <div className="carousel"> 
+                <h2>What our customers say</h2>
+                <Carousel>
+                <div>
+                    <img src={me} />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src={me} />
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <img src={me} />
+                    <p className="legend">Legend 3</p>
+                </div>
+                </Carousel>
+            </div>
+        </Fragment>
+    )
+}
