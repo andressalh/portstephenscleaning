3f224d5a4dde7b506c98b0673254e36d37abcdfa
andressalh <andressalhenriques@hotmail.com>
2020-06-12 19:43:52 -0300

    update

--- portstephenscleaning/src/pages/Services/index.js
+++ portstephenscleaning/src/pages/Services/index.js
@@ -1,36 +1,54 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import './styles.css';
-
-import {
-    CardImg,
-    CardBody,
-    CardTitle,
-    CardText,
-    Card,
-    NavLink,
-  } from 'reactstrap';
-
+import Header from '../../components/header';
+import Footer from '../../components/footer';
+import products from '../../assets/products.png';
 
 export default function Portfolio() {
 
     return (
       
-        <div className="services"> 
+        <Fragment>
+            <Header/>
 
-            <Card className="cardServices" >
-              <div className="divImg">
-                <CardImg variant="top" src="" />    
-              </div>
-              <CardBody className="cardBodyServices">
-                <CardTitle>Pokedex</CardTitle>
-                <CardText>
-                  Design  / react / api
-                </CardText>
-              </CardBody>
-            </Card>
+            <div className="services">
+                <h2>Services</h2>
+                <div className="service service-right "> 
+                    <div className="div-img-service">
+                        <img src={products} alt="products" />
+                    </div>
+                    <div>
+                        <h4>RESIDENTIAL CLEANING</h4>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    </div>
+                </div>
 
-           
-        </div>
+                <div className="service service-left "> 
+                    
+                    <div>
+                        <h4>RESIDENTIAL CLEANING</h4>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    </div>
+                    <div className="div-img-service">
+                        <img src={products} alt="products" />
+                    </div>
+                </div>
+
+                <div className="service service-right "> 
+                    <div className="div-img-service">
+                        <img src={products} alt="products" />
+                    </div>
+                    <div>
+                        <h4>RESIDENTIAL CLEANING</h4>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    </div>
+                </div>
+                
+            </div>
+            
+            <Footer/>
+            
+        </Fragment>
     );
    
   }
