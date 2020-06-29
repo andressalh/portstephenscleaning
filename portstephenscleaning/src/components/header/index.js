9ad31a62bc0456debbf212e75f985b58fbcfedb3
andressalh <andressalhenriques@hotmail.com>
2020-06-29 11:45:35 -0300

    seo google

--- portstephenscleaning/src/components/header/index.js
+++ portstephenscleaning/src/components/header/index.js
@@ -11,7 +11,7 @@ export default function Header (){
         <Fragment>
             <div className="header">
             <Navbar collapseOnSelect expand="lg" className="header-show fixed-top" id="header-show">
-                <Navbar.Brand href="/"><img className="logoHeader" src={logo} Port Stephens Cleaning Services room /></Navbar.Brand>
+                <Navbar.Brand href="/"><img className="logoHeader" src={logo} alt="Port Stephens Cleaning Services room" /></Navbar.Brand>
                 <h1>Port Stephens Cleaning Services</h1>
                 </Navbar>
                 <div className="header-image"></div>
